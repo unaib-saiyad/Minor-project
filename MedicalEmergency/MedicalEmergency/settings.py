5f0d270985e7db0f028f48032c5bca9f8cc3167e
unaib-saiyad <unaibsaiyad18@gmail.com>
2022-03-25 15:30:15 +0530

    Server setup

--- MedicalEmergency/MedicalEmergency/settings.py
+++ MedicalEmergency/MedicalEmergency/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'MedicalEmergency.urls'
@@ -130,6 +131,8 @@ STATIC_URL = 'static/'
 # Add these new lines
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Media files setup
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
